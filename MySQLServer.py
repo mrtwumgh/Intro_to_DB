c5d7154bf580c80457567936d2b5a2d2647d47e0
mrtwumgh <twumstephen2006@gmail.com>
2024-07-28 07:45:54 +0000

    add: MySQLServer.py added

---
+++ MySQLServer.py
@@ -0,0 +1,17 @@
+import mysql.connector
+try:
+    mydb = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='mypass'
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+
+except DatabaseError:
+    print("An error occurred when establishing a connection")
+finally:
+    mycursor.close()
+    mydb.close()
